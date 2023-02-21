f5178e246fb89d5567d13bf15fe72ff2ea573e39
zmkhtr <azammukhtar3@gmail.com>
2023-02-21 09:55:12 +0700

    Initial Commit

---
+++ Games/Games.h
@@ -0,0 +1,18 @@
+//
+//  Games.h
+//  Games
+//
+//  Created by Azam Mukhtar on 21/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Games.
+FOUNDATION_EXPORT double GamesVersionNumber;
+
+//! Project version string for Games.
+FOUNDATION_EXPORT const unsigned char GamesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Games/PublicHeader.h>
+
+
