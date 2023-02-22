cf2c7174935b36aae58aacff9c609580aeb7bd8a
zmkhtr <azammukhtar3@gmail.com>
2023-02-22 10:39:00 +0700

    Create Games Item and Games Loader with Games Request

---
+++ Games/Games Feature/GameItem.swift
@@ -0,0 +1,16 @@
+//
+//  GameItem.swift
+//  Games
+//
+//  Created by Azam Mukhtar on 22/02/23.
+//
+
+import Foundation
+
+public struct GameItem {
+    let id: Int
+    let title: String
+    let releaseDate: String
+    let rating: String
+    let image: URL
+}
