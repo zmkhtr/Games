a7f21540368db9ee834d6af391775c221721e0f4
zmkhtr <azammukhtar3@gmail.com>
2023-02-22 11:05:22 +0700

    Move GamesEndpoint to production

--- GamesTests/Games API/GamesEndpointTests.swift
+++ GamesTests/Games API/GamesEndpointTests.swift
@@ -8,36 +8,6 @@
 import XCTest
 import Games
 
-public enum GamesEndpoint {
-    case get(request: GamesRequest)
-    
-    public func url(baseURL: URL) -> URLRequest {
-        switch self {
-        case let .get(request):
-            var components = URLComponents()
-            components.scheme = baseURL.scheme
-            components.host = baseURL.host
-            components.path = baseURL.path
-            
-            guard let query = request.search else {
-                components.queryItems = [
-                    URLQueryItem(name: "page", value: "\(request.page)"),
-                    URLQueryItem(name: "page_size", value: "\(request.page_size)"),
-                ].compactMap { $0 }
-                return URLRequest(url: components.url!)
-            }
-         
-            components.queryItems = [
-                URLQueryItem(name: "page", value: "\(request.page)"),
-                URLQueryItem(name: "page_size", value: "\(request.page_size)"),
-                URLQueryItem(name: "search", value: "\(query)"),
-            ].compactMap { $0 }
-            return URLRequest(url: components.url!)
-        }
-    }
-}
-
-
 class GamesEndpointTests: XCTestCase {
     
     func test_games_endpointURL() {
