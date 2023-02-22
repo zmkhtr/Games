b687072ea6f7e3863bfcee83910587289dfe480f
zmkhtr <azammukhtar3@gmail.com>
2023-02-22 14:35:34 +0700

    Create Game Detail Loader and Game Detail Item

---
+++ Games/Games Detail Feature/GameDetailItem.swift
@@ -0,0 +1,32 @@
+//
+//  GameDetailItem.swift
+//  Games
+//
+//  Created by Azam Mukhtar on 22/02/23.
+//
+
+import Foundation
+
+public struct GameDetailItem: Hashable {
+    public let id: Int
+    public let title: String
+    public let releaseDate: String
+    public let rating: Double
+    public let image: URL
+    public let description: String
+    public let played: Int
+    public let developers: String
+    public let isFavorite: Bool
+    
+    public init(id: Int, title: String, releaseDate: String, rating: Double, image: URL, description: String, played: Int, developers: String, isFavorite: Bool) {
+        self.id = id
+        self.title = title
+        self.releaseDate = releaseDate
+        self.rating = rating
+        self.image = image
+        self.description = description
+        self.played = played
+        self.developers = developers
+        self.isFavorite = isFavorite
+    }
+}
