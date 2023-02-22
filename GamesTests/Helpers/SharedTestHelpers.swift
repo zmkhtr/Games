cf7ea0367db79238ee366d72e906a0b9a3aa91e2
zmkhtr <azammukhtar3@gmail.com>
2023-02-22 11:38:34 +0700

    Create Shared TestHelpers

---
+++ GamesTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,12 @@
+//
+//  SharedTestHelpers.swift
+//  GamesTests
+//
+//  Created by Azam Mukhtar on 22/02/23.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
