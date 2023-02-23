1922dbf0f66de9ab38eaf381a67d6b7a1f427936
zmkhtr <azammukhtar3@gmail.com>
2023-02-23 09:36:53 +0700

    Add Games App Target

---
+++ Games AppUITests/Games_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Games_AppUITestsLaunchTests.swift
+//  Games AppUITests
+//
+//  Created by Azam Mukhtar on 23/02/23.
+//
+
+import XCTest
+
+final class Games_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
